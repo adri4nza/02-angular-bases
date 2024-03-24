b93f2a3dc429c5669d87656b864a953a0c37ff25
Francisco Adrianza <fadrianza2101@gmail.com>
2024-03-24 02:12:48 -0400

    80 github pages

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -25,7 +25,6 @@ export class DbzService {
 
   addCharacter(character: Character): void {
     const newCharacter: Character = { ...character, id: uuid() };
-
     this.characters.push(newCharacter);
   }
 
